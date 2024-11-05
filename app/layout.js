d1a23916614a263c8a7430995bf96d0348f107d8
Tanjimul Islam <tanjimul.islam@hotmail.com>
2024-11-05 17:54:27 +0600

    noscript

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
       </Script>
       <body className={`antialiased `}>
         {/* NoScript for Facebook Pixel */}
-        {/* <noscript>
+        <noscript>
           <img
             height="1"
             width="1"
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
             src="https://www.facebook.com/tr?id=1057799606043862&ev=PageView&noscript=1"
             alt="Facebook Pixel"
           />
-        </noscript> */}
+        </noscript>
         {children}
       </body>
     </html>
