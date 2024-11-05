2cf561c88e3fc62395f5c5f8dfa44c9f6e366f0e
Tanjimul Islam <tanjimul.islam@hotmail.com>
2024-11-05 17:14:26 +0600

    pixel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Noto_Sans_Bengali } from "next/font/google";
-
+import Head from "next/head";
 import "./globals.css";
+import Script from "next/script";
 const notoSansBengali = Noto_Sans_Bengali({
   subsets: ["bengali"],
   weight: ["400", "700"],
@@ -14,23 +15,37 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={notoSansBengali.className}>
+      <Head>
+        {/* Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1057799606043862');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </Head>
       <body className={`antialiased `}>
-      <script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1057799606043862');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1057799606043862&ev=PageView&noscript=1"
-/></noscript>
-        {children}</body>
+        {children}
+        {/* NoScript for Facebook Pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1057799606043862&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
