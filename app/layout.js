4577c454d9d74df8a0962776a4943f0696fa0917
Tanjimul Islam <tanjimul.islam@hotmail.com>
2024-11-05 19:01:53 +0600

    up

--- app/layout.js
+++ app/layout.js
@@ -15,21 +15,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={notoSansBengali.className}>
-<Script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1057799606043862');
-fbq('track', 'PageView');
-</Script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1057799606043862&ev=PageView&noscript=1"
-/></noscript>
+      <Head>
+        <title>Your Page Title</title>
+        {/* Inline script using dangerouslySetInnerHTML */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1057799606043862');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </Head>
       <body className={`antialiased `}>
         {/* NoScript for Facebook Pixel */}
         <noscript>
